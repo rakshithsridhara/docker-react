a63436c4231642c056bc79cced539fa3eed86d1c
Rakshith <rakshith.sri90@gmail.com>
2021-09-17 21:12:25 -0400

    changed app test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi John!!!
+          Hi John feature branch!!!
         </p>
         <a
           className="App-link"
